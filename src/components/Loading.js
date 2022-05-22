9ca7887859975e1795cbea6fca913f08040eb4cd
Prometheus <96538997+MoanaSeeha@users.noreply.github.com>
2022-05-22 09:54:20 +0800

    888

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Oval} from 'react-loading-icons'
+
+const Loading = () => {
+    return (
+      <div className='loading_panel'>
+        <Oval className='loading' />
+      </div>
+    )
+}
+
+export default Loading;
